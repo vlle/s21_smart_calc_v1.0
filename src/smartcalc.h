7d2136ff83a75461f0f9b2bfc3dab9418f3faf69
artemiikulikov <aapqwhat@gmail.com>
2022-09-12 22:55:05 +0300

    :shirt: update(stack): stack WIP

---
+++ src/smartcalc.h
@@ -0,0 +1,11 @@
+#ifndef SRC_SMARTCALC_H_
+#define SRC_SMARTCALC_H_
+
+typedef struct stack
+{
+    int maxsize;    // define max capacity of the stack
+    int top;
+    int *items;
+} math_stack;
+
+#endif
---
+++ src/stack/stack.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+#include "../smartcalc.h"
+
+int pop(math_stack *a) {
+  return 1;
+
+}
+
+int push_back(math_stack *a) {
+  return 1;
+}
+
+int main() {
+  return 0;
+}
