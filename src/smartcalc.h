2234998fb168bdad4ecbe59679ab68055a5cdd33
artemiikulikov <aapqwhat@gmail.com>
2022-09-16 18:13:04 +0300

    :shirt: update(stack): stack WIP (i think its done though)

--- src/smartcalc.h
+++ src/smartcalc.h
@@ -2,42 +2,26 @@
 #define SRC_SMARTCALC_H_
 
 typedef enum {
-    minus,
-    plus,
-    multiply,
-    divide,
-    sinus,
-    cosinus,
-    tangengs,
-    etc
-} math_oper ;
-
-typedef struct stack
-{
-    int storage;
-    struct stack *next;
-} math_stack;
+  minus,
+  plus,
+  multiply,
+  divide,
+  sinus,
+  cosinus,
+  tangengs,
+  etc
+} math_oper;
 
 struct Node {
-    int storage;               // integer data
-    struct Node* next;      // pointer to the next node
+  int storage;        // integer data
+  struct Node *next;  // pointer to the next node
 };
 
 // Stack functions
 
-struct stack* init_stack(int value);
 int peek(struct Node *a);
 int pop(int *nodesCount, struct Node **a);
 int push_back(int *nodesCount, struct Node **a, int oper);
 int IsEmpty(struct Node *top);
-
-
-
-
-
-
-
-
-
 
 #endif
--- src/stack/stack.c
+++ src/stack/stack.c
@@ -1,20 +1,12 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "../smartcalc.h"
 
-
-struct stack* init_stack(int value) {
-  math_stack *a = (math_stack*) malloc(sizeof(math_stack));
-  a->storage = value;
-  a->next = NULL;
-  return a;
-}
-
 int push_back(int *nodesCount, struct Node **top, int oper) {
-  struct Node* newNode = NULL;
-  newNode = (struct Node*) malloc(sizeof(struct Node));
-  newNode->storage = 10;
+  struct Node *newNode = NULL;
+  newNode = (struct Node *)malloc(sizeof(struct Node));
+  newNode->storage = oper;
   if (top == NULL) {
     newNode->next = NULL;
     printf("%d = aaa\n", 0);
@@ -27,40 +19,34 @@ int push_back(int *nodesCount, struct Node **top, int oper) {
   return 0;
 }
 
+int peek(struct Node *a) { return a->storage; }
 
-int peek(struct Node *a) {
-  return a->storage;
-}
-
-int pop(int *nodesCount, struct Node**top) {
-  struct Node* node;
+int pop(int *nodesCount, struct Node **top) {
+  int x = -1;
+  struct Node *node;
   if (IsEmpty(*top)) {
     printf("Top stack is empty");
   } else {
-    int x = peek(nodesCount, *top)
-    math_stack *tmp = top;
-    oper = top->storage;
-    top = tmp->next;
-    free(tmp);
+    x = peek(*top);
+    node = *top;
+    *top = (*top)->next;
+    *nodesCount -= 1;
+    free(node);
   }
-  return 0;
+  return x;
 }
 
-int IsEmpty(struct Node *top) {
-  return top == NULL;
-}
+int IsEmpty(struct Node *top) { return top == NULL; }
 // IsFull
-
-
 
 int main() {
   struct Node *top;
   int nodesCount = 0;
-  // top = init_stack(5);
-  int a = 10, b = 20, c = 30;
-  push_back(&nodesCount, &top, a);
-  // top->storage++;
-  printf("%d is top ", top->storage);
-  // printf("%d", pop(top));
+  push_back(&nodesCount, &top, 30);
+  push_back(&nodesCount, &top, 32);
+  push_back(&nodesCount, &top, 35);
+  printf("%d\n", pop(&nodesCount, &top));
+  printf("%d\n", pop(&nodesCount, &top));
+  printf("%d\n", pop(&nodesCount, &top));
   return 0;
 }
