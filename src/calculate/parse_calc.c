b1b87e24e4b23426f0e3ec45e9bcebee83ae4eec
Worker Corrino <workerco@su-e5.msk.21-school.ru>
2022-09-19 17:22:18 +0300

    :bug: update(parse_calc): add malloc and malloc prot

--- src/calculate/parse_calc.c
+++ src/calculate/parse_calc.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #include "../smartcalc.h"
+#define MAX_INP_SZ 256
 
 void strcat_c (char *str, char c) {
   for (;*str;str++); // note the terminating semicolon here. 
@@ -10,12 +11,18 @@ void strcat_c (char *str, char c) {
   *str++ = 0;
 }
 
-char* parse_oper(char* inpstr, char* funcstr) {
+char* parse_oper(char* funcstr) {
   struct Node* opr;
+  char *inpstr = (char*) malloc(MAX_INP_SZ);
+  if (!inpstr) {
+    fprintf(stderr, "No memory alloc. \n");
+    perror("");
+  }
   int nodesCount = 0;
   int nodesCount1 = 0;
   int funcstr_i = 0;
-  scanf("%s", inpstr);
+  fgets(inpstr, MAX_INP_SZ, stdin);
+  char* start = inpstr;
   int numcount = 0;
   for (; *inpstr != '\0'; inpstr++) {
       printf("%c iS curr\n", *inpstr);
@@ -24,11 +31,14 @@ char* parse_oper(char* inpstr, char* funcstr) {
       char *pEnd;
       long double calc_num = strtol(inpstr, &pEnd, 10);
       sprintf(tmp, "%.0Lf", calc_num);
+      printf("%c iS curr\n", *inpstr);
       inpstr = pEnd;
+      printf("%c iS curr\n", *inpstr);
       // strncat(funcstr, tmp, strlen(tmp));
       strcat(funcstr, tmp);
       strcat(funcstr, " ");
       numcount++;
+      // store in funciton?
       // while num parse
       // TODO(artemii): error handling
     } 
@@ -45,14 +55,14 @@ char* parse_oper(char* inpstr, char* funcstr) {
   int tmp = nodesCount1;
   funcstr_i = strlen(funcstr);
   while (tmp > 0) {
+    printf("%d is tmp\n", tmp);
     funcstr[funcstr_i] = popC(&tmp, &opr); // peek for preceding op
     funcstr_i++;
     funcstr[funcstr_i] = ' ';
     funcstr_i++;
-    // strcat_c(funcstr, popC(&tmp, &opr));
-    // strcat(funcstr, " ");
   }
   funcstr[funcstr_i] = '\0';
+  free(start);
   return funcstr;
 }
 
@@ -69,21 +79,29 @@ long double cal_oper(char* funcstr) {
       push_backN(&nodesCount, &nums, calc_num);
       funcstr = pEnd;
     } else if (*funcstr == '+') {
+      if (nodesCount > 1) {
       int a = popN(&nodesCount, &nums);
       int b = popN(&nodesCount, &nums);
       result = a + b;
+      } else {
+        result = -1;
+        while (nodesCount) {
+          popN(&nodesCount, &nums);
+        }
+        fprintf(stderr, "Wrong calculation. Returning -1\n");
+      }
     }
   }
   return result;
 }
 
 int main() {
-  char inpstr[100] = {0};
   char funcstr[400] = {0};
   char *go;
-  go = parse_oper(inpstr, funcstr);
+  go = parse_oper(funcstr);
   printf("%s", go);
-  long double res = cal_oper(go);
+  long double res = 0;
+  res = cal_oper(go);
   printf("\n%Lf", res);
   return 0;
 }
