35f89f0c3b3be4745ccf74f0cde89ddd8cb33193
artemiikulikov <aapqwhat@gmail.com>
2022-11-01 20:44:48 +0400

    :white_check_mark: tests(add-min): +2 mediocre

--- src/tests/add-min.c
+++ src/tests/add-min.c
@@ -17,6 +17,36 @@ START_TEST(add_braces) {
   char b[100] = "2 + (2 + 10)";
   double res = 14;
   char *prs = parse_oper(funcstr, b);
+  double my_res = cal_oper(prs); ck_assert_double_eq(res, my_res);
+}
+END_TEST
+
+START_TEST(min) {
+  char funcstr[40] = {0};
+  char b[100] = "2 - 2";
+  double res = 0;
+  char *prs = parse_oper(funcstr, b);
+  double my_res = cal_oper(prs);
+  ck_assert_double_eq(res, my_res);
+}
+END_TEST
+
+START_TEST(min_min) {
+  char funcstr[40] = {0};
+  // char b[100] = "6-2-2";
+  char b[100] = "6 + 2 - 2";
+  double res = 6;
+  char *prs = parse_oper(funcstr, b);
+  double my_res = cal_oper(prs);
+  ck_assert_double_eq(res, my_res);
+}
+END_TEST
+
+START_TEST(min_mid) {
+  char funcstr[40] = {0};
+  char b[100] = "5 - 2";
+  double res = 3;
+  char *prs = parse_oper(funcstr, b);
   double my_res = cal_oper(prs);
   ck_assert_double_eq(res, my_res);
 }
@@ -41,6 +71,9 @@ Suite *add_min(void) {
   tcase_add_test(tc_core, add_braces);
   tcase_add_test(tc_core, add_minus);
   tcase_add_test(tc_core, add);
+  tcase_add_test(tc_core, min);
+  tcase_add_test(tc_core, min_min);
+  tcase_add_test(tc_core, min_mid);
   suite_add_tcase(s, tc_core);
 
   return s;
