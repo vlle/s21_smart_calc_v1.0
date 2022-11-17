bdebd10ff05a307cefee5f0de37fca402470417c
Worker Corrino <workerco@mi-e1.msk.21-school.ru>
2022-11-17 21:23:19 +0300

    :star: update(calc): add basic credit calculate

---
+++ src/calculate/s21_bank_calc.c
@@ -0,0 +1,30 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../smartcalc.h"
+
+int credit_calculate() {
+  long double total_credit_amount, monthly_payment;
+  long double term, total_payment, overpayment;
+  long double interest_rate;
+  char type = 'a';
+  total_credit_amount = 100000.0;
+  term = 5.0*12.0;
+  interest_rate = 0.15;
+  total_payment = total_credit_amount + \
+                    total_credit_amount * term \
+                    * interest_rate;
+  monthly_payment = total_payment / term;
+  overpayment = total_payment - total_credit_amount;
+
+  printf("%Lf = total\n%Lf = monthly\n%Lf = overpay",\
+      total_payment, monthly_payment, overpayment);
+  // result = (total + total*interest*term)
+}
+
+int main() {
+  credit_calculate();
+  return 0;
+}
