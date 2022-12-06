83837c2da031aa498f5344b4113a9b2b43cca311
Worker Corrino <workerco@su-d5.msk.21-school.ru>
2022-12-06 18:31:48 +0300

    :shirt: update(gui): wip entry

--- src2/gui/gtk_ent.c
+++ src2/gui/gtk_ent.c
@@ -1,4 +1,13 @@
 #include <gtk/gtk.h>
+#include "../smartcalc.h"
+
+static void 
+calculat (GtkWidget* widget, gpointer data)
+{
+  (void) widget;
+  (void) data;
+  g_print("Hello A\n");
+}
 
 static void
 activate (GtkApplication* app,
@@ -6,16 +15,39 @@ activate (GtkApplication* app,
 {
   (void)user_data;
   GtkWidget *window;
-  GtkWidget *entry_text;
-  GtkWidget *text_box_H;
+  GtkWidget *entry_text, *label_text;
+  GtkWidget *text_box_H, *text_box_V, *text_box_Hor;
+  GtkWidget *grid;
+  GtkWidget *button_calc, *button_q;
 
   window = gtk_application_window_new (app);
   entry_text = gtk_entry_new();
-  text_box_H = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
+  label_text = gtk_label_new("Enter your infix string: ");
+  text_box_V = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+  text_box_H = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+  text_box_Hor = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
+  grid = gtk_grid_new ();
+  button_calc = gtk_button_new_with_label ("=");
+  button_q = gtk_button_new_with_label ("Quit");
+  g_signal_connect (button_calc, "clicked", G_CALLBACK(calculat), NULL); 
+  g_signal_connect_swapped (button_q, "clicked", G_CALLBACK(gtk_window_destroy), GTK_WINDOW(window)); 
+  gtk_grid_attach (GTK_GRID (grid), button_calc, 1, 0, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid), button_q, 0, 1, 2, 1);
+  gtk_box_set_homogeneous(GTK_BOX(text_box_H), FALSE);
+  // gtk_widget_set_valign(text_box_V, GTK_ALIGN_CENTER);
+  // gtk_widget_set_valign(text_box_H, GTK_ALIGN_CENTER);
+  // gtk_widget_set_valign(text_box_Hor, GTK_ALIGN_CENTER);
+  gtk_widget_set_halign(text_box_V, GTK_ALIGN_CENTER);
+  gtk_widget_set_halign(text_box_H, GTK_ALIGN_CENTER);
+  gtk_widget_set_halign(text_box_Hor, GTK_ALIGN_CENTER);
+  gtk_box_prepend(GTK_BOX(text_box_V), GTK_WIDGET(label_text));
   gtk_box_prepend(GTK_BOX(text_box_H), GTK_WIDGET(entry_text));
-  gtk_window_set_child(GTK_WINDOW (window), text_box_H);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_box_prepend(GTK_BOX(text_box_Hor), GTK_WIDGET(text_box_H));
+  gtk_box_prepend(GTK_BOX(text_box_Hor), GTK_WIDGET(grid));
+  gtk_box_prepend(GTK_BOX(text_box_Hor), GTK_WIDGET(text_box_V));
+  gtk_window_set_child(GTK_WINDOW (window), text_box_Hor);
+  gtk_window_set_title (GTK_WINDOW (window), "Smartcalc Artemii");
+  gtk_window_set_default_size (GTK_WINDOW (window), 400, 400);
   gtk_widget_show (window);
 }
 
