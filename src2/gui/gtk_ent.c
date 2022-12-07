38c6dffc6bd880d87d2f815e0aa3f6d3bc318ccf
artemiikulikov <aapqwhat@gmail.com>
2022-12-07 20:00:48 +0300

    :shirt: update(gui): add numbs

--- src2/gui/gtk_ent.c
+++ src2/gui/gtk_ent.c
@@ -2,6 +2,8 @@
 #include "../smartcalc.h"
 #include "gui.h"
 
+GtkEntryBuffer *entry_buff;
+
 static void 
 calculat (GtkWidget* widget, gpointer data)
 {
@@ -16,6 +18,17 @@ calculat (GtkWidget* widget, gpointer data)
 }
 
 static void
+insert_text(GtkWidget*widget, gpointer data) {
+  (void) widget;
+  calc*input = (calc*) data;
+  // gtk_button_get_label
+  g_print("%s\n", gtk_button_get_label(GTK_BUTTON(widget)));
+  int f = gtk_entry_buffer_get_length(GTK_ENTRY_BUFFER(input->buff));
+  const char *text = gtk_button_get_label(GTK_BUTTON(widget));
+  gtk_entry_buffer_insert_text(GTK_ENTRY_BUFFER(input->buff),f+1, text, 1);
+}
+
+static void
 activate (GtkApplication* app,
           gpointer        user_data)
 {
@@ -26,11 +39,6 @@ activate (GtkApplication* app,
   calc *calc_data = malloc(sizeof(*calc_data));
   GtkWidget *grid_numb;
   GtkWidget *button_calc, *button_q;
-  GtkWidget *n1_button, *n2_button, *n3_button, *n4_button;
-  GtkWidget *n5_button, *n6_button, *n7_button, *n8_button;
-  GtkWidget *n9_button, *n0_button, *fl_button;
-  GtkWidget *plus_button, *minus_button, *mult_button, *div_button;
-  GtkWidget *sin_button, *cos_button, *tan_button;
 
   window = gtk_application_window_new (app);
   calc_data->entry_text = gtk_entry_new();
@@ -43,54 +51,77 @@ activate (GtkApplication* app,
   text_box_Vert = gtk_box_new(GTK_ORIENTATION_VERTICAL, 30);
   text_box_Grid = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 30);
   grid_numb = gtk_grid_new ();
+  entry_buff = gtk_entry_buffer_new(NULL, -1);
+  calc_data->buff = entry_buff;
   button_calc = gtk_button_new_with_label ("=");
-  n1_button = gtk_button_new_with_label ("1");
-  n2_button = gtk_button_new_with_label ("2");
-  n3_button = gtk_button_new_with_label ("3");
-  n4_button = gtk_button_new_with_label ("4");
-  n5_button = gtk_button_new_with_label ("5");
-  n6_button = gtk_button_new_with_label ("6");
-  n7_button = gtk_button_new_with_label ("7");
-  n8_button = gtk_button_new_with_label ("8");
-  n9_button = gtk_button_new_with_label ("9");
-  n0_button = gtk_button_new_with_label ("0");
-  fl_button = gtk_button_new_with_label (",");
-  sin_button = gtk_button_new_with_label ("sin");
-  cos_button = gtk_button_new_with_label ("cos");
-  tan_button = gtk_button_new_with_label ("tan");
-  div_button = gtk_button_new_with_label ("÷");
-  plus_button = gtk_button_new_with_label ("+");
-  minus_button = gtk_button_new_with_label ("-");
-  mult_button = gtk_button_new_with_label ("X");
+  calc_data->n1_button = gtk_button_new_with_label ("1");
+  calc_data->n2_button = gtk_button_new_with_label ("2");
+  calc_data->n3_button = gtk_button_new_with_label ("3");
+  calc_data->n4_button = gtk_button_new_with_label ("4");
+  calc_data->n5_button = gtk_button_new_with_label ("5");
+  calc_data->n6_button = gtk_button_new_with_label ("6");
+  calc_data->n7_button = gtk_button_new_with_label ("7");
+  calc_data->n8_button = gtk_button_new_with_label ("8");
+  calc_data->n9_button = gtk_button_new_with_label ("9");
+  calc_data->n0_button = gtk_button_new_with_label ("0");
+  calc_data->fl_button = gtk_button_new_with_label (",");
+  calc_data->sin_button = gtk_button_new_with_label ("sin");
+  calc_data->cos_button = gtk_button_new_with_label ("cos");
+  calc_data->tan_button = gtk_button_new_with_label ("tan");
+  calc_data->div_button = gtk_button_new_with_label ("÷");
+  calc_data->plus_button = gtk_button_new_with_label ("+");
+  calc_data->minus_button = gtk_button_new_with_label ("-");
+  calc_data->mult_button = gtk_button_new_with_label ("X");
   button_q = gtk_button_new_with_label ("Quit");
+  gtk_entry_set_buffer(GTK_ENTRY(calc_data->entry_text), GTK_ENTRY_BUFFER(entry_buff));
   g_signal_connect (button_calc, "clicked", G_CALLBACK(calculat), calc_data); 
   g_signal_connect_swapped (button_q, "clicked", G_CALLBACK(gtk_window_destroy), GTK_WINDOW(window)); 
   // gtk_grid_attach (GTK_GRID (grid_numb), button_calc, 1, 1, 1, 1);
   // gtk_grid_attach (GTK_GRID (grid_numb), button_q, 1, 2, 1, 1);
 
-  gtk_grid_attach (GTK_GRID (grid_numb), n1_button, 1, 3, 1, 1);
-  gtk_grid_attach (GTK_GRID (grid_numb), n2_button, 2, 3, 1, 1);
-  gtk_grid_attach (GTK_GRID (grid_numb), n3_button, 3, 3, 1, 1);
-  gtk_grid_attach (GTK_GRID (grid_numb), plus_button, 4, 3, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->n1_button, 1, 4, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->n2_button, 2, 4, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->n3_button, 3, 4, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->plus_button, 4, 4, 1, 1);
 
-  gtk_grid_attach (GTK_GRID (grid_numb), n4_button, 1, 2, 1, 1);
-  gtk_grid_attach (GTK_GRID (grid_numb), n5_button, 2, 2, 1, 1);
-  gtk_grid_attach (GTK_GRID (grid_numb), n6_button, 3, 2, 1, 1);
-  gtk_grid_attach (GTK_GRID (grid_numb), minus_button, 4, 2, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->n4_button, 1, 3, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->n5_button, 2, 3, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->n6_button, 3, 3, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->minus_button, 4, 3, 1, 1);
 
-  gtk_grid_attach (GTK_GRID (grid_numb), n7_button, 1, 1, 1, 1);
-  gtk_grid_attach (GTK_GRID (grid_numb), n8_button, 2, 1, 1, 1);
-  gtk_grid_attach (GTK_GRID (grid_numb), n9_button, 3, 1, 1, 1);
-  gtk_grid_attach (GTK_GRID (grid_numb), mult_button, 4, 1, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->n7_button, 1, 2, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->n8_button, 2, 2, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->n9_button, 3, 2, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->mult_button, 4, 2, 1, 1);
 
-  gtk_grid_attach (GTK_GRID (grid_numb), sin_button, 1, 1, 1, 1);
-  gtk_grid_attach (GTK_GRID (grid_numb), cos_button, 2, 1, 1, 1);
-  gtk_grid_attach (GTK_GRID (grid_numb), tan_button, 3, 1, 1, 1);
-  gtk_grid_attach (GTK_GRID (grid_numb), div_button, 4, 1, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->sin_button, 1, 1, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->cos_button, 2, 1, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->tan_button, 3, 1, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->div_button, 4, 1, 1, 1);
+  g_signal_connect (calc_data->n1_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  g_signal_connect (calc_data->n2_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  g_signal_connect (calc_data->n3_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  g_signal_connect (calc_data->n4_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  g_signal_connect (calc_data->n5_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  g_signal_connect (calc_data->n6_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  g_signal_connect (calc_data->n7_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  g_signal_connect (calc_data->n8_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  g_signal_connect (calc_data->n9_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  g_signal_connect (calc_data->n0_button, "clicked", G_CALLBACK(insert_text), calc_data); 
 
-  gtk_grid_attach (GTK_GRID (grid_numb), n0_button, 1, 4, 2, 1);
-  gtk_grid_attach (GTK_GRID (grid_numb), fl_button, 3, 4, 1, 1);
-  gtk_grid_attach (GTK_GRID (grid_numb), button_calc, 4, 4, 1, 1);
+  g_signal_connect (calc_data->plus_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  g_signal_connect (calc_data->minus_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  g_signal_connect (calc_data->mult_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  g_signal_connect (calc_data->div_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  g_signal_connect (calc_data->sin_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  g_signal_connect (calc_data->cos_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  g_signal_connect (calc_data->tan_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  // g_signal_connect (calc_data->sin_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+  // g_signal_connect (calc_data->n0_button, "clicked", G_CALLBACK(insert_text), calc_data); 
+
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->n0_button, 1, 5, 2, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), calc_data->fl_button, 3, 5, 1, 1);
+  gtk_grid_attach (GTK_GRID (grid_numb), button_calc, 4, 5, 1, 1);
   gtk_box_set_homogeneous(GTK_BOX(text_box_H), FALSE);
   gtk_widget_set_halign(calc_data->entry_text, GTK_ALIGN_CENTER);
   gtk_widget_set_halign(calc_data->label_empty, GTK_ALIGN_CENTER);
@@ -120,7 +151,7 @@ activate (GtkApplication* app,
   gtk_box_prepend(GTK_BOX(text_box_Vert), GTK_WIDGET(text_box_Hor));
   gtk_window_set_child(GTK_WINDOW (window), text_box_Vert);
   gtk_window_set_title (GTK_WINDOW (window), "Smartcalc Artemii");
-  gtk_window_set_default_size (GTK_WINDOW (window), 250, 250);
+  gtk_window_set_default_size (GTK_WINDOW (window), 250, 300);
   gtk_widget_show (window);
 }
 
--- src2/gui/gui.h
+++ src2/gui/gui.h
@@ -4,6 +4,30 @@
 typedef struct calc {
   GtkWidget *label_empty;
   GtkWidget *entry_text;
+  int i;
+  GtkWidget *n1_button;
+  GtkWidget *n2_button;
+  GtkWidget *n3_button;
+  GtkWidget *n4_button;
+  GtkWidget *n5_button;
+  GtkWidget *n6_button;
+  GtkWidget *n7_button;
+  GtkWidget *n8_button;
+  GtkWidget *n9_button;
+  GtkWidget *n0_button;
+  GtkWidget *fl_button;
+  GtkWidget *sin_button;
+  GtkWidget *cos_button;
+  GtkWidget *tan_button;
+  GtkWidget *div_button;
+  GtkWidget *plus_button;
+  GtkWidget *minus_button;
+  GtkWidget *mult_button;
+
+  GtkWidget *buttSq;
+  GtkWidget *buttL;
+  GtkWidget *buttl;
+  GtkEntryBuffer *buff;
 } calc;
 
 #endif // SRC_GUI_GUI_H_
